151cc64c4634004a45f0e26644143969b82c9f7c
Dio Ilham <apajabole1@gmail.com>
2021-07-29 04:31:11 +0700

    #1: idk just fixing this s2pid tailwindcss jit bug. Idk is this solved or nah...

--- 1-Lacrime/client/craco.config.js
+++ 1-Lacrime/client/craco.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+
 module.exports = {
   style: {
     postcss: {
--- 1-Lacrime/client/tailwind.config.js
+++ 1-Lacrime/client/tailwind.config.js
@@ -3,26 +3,10 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
+  // mode: "jit",
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-
-      black: colors.black,
-      white: colors.white,
-      gray: colors.blueGray,
-      // red: colors.red,
-      orange: colors.orange,
-      // yellow: colors.amber,
-      // green: colors.emerald,
-      // blue: colors.blue,
-      indigo: colors.indigo,
-      // purple: colors.violet,
-      // pink: colors.pink,
-    },
     fontFamily: {
       st: [
         "Inter",
@@ -58,10 +42,29 @@ module.exports = {
         '"Noto Color Emoji"',
       ],
     },
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+
+        black: colors.black,
+        white: colors.white,
+        gray: colors.blueGray,
+        // red: colors.red,
+        orange: colors.orange,
+        yellow: colors.amber,
+        // green: colors.emerald,
+        // blue: colors.blue,
+        // indigo: colors.indigo,
+        // purple: colors.violet,
+        // pink: colors.pink,
+        rose: colors.rose,
+      },
+    },
   },
   variants: { extend: {}, },
   plugins: [
+    // require("@tailwindcss/jit"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
   ],
