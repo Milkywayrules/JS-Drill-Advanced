c3718eed49b333a31df0caf92354ca272f37803a
Dio Ilham <apajabole1@gmail.com>
2021-08-04 05:25:07 +0700

    #1: Generate some of graphql query

--- 1-Lacrime/client/src/pages/Graphql.tsx
+++ 1-Lacrime/client/src/pages/Graphql.tsx
@@ -1,73 +1,37 @@
-/* eslint-disable object-curly-newline */
-import { gql, useQuery } from '@apollo/client';
-import React from 'react';
+import React, { useState } from 'react';
+import { useGetTrendingAnimeQuery } from '../generated/graphql';
 
 interface Props {}
 
-const GET_ANIME = gql`
-  query ($id: Int, $page: Int, $perPage: Int, $search: String) {
-    Page(page: $page, perPage: $perPage) {
-      pageInfo {
-        total
-        currentPage
-        lastPage
-        hasNextPage
-        perPage
-      }
-      media(id: $id, search: $search) {
-        id
-        title {
-          romaji
-        }
-      }
-    }
-  }
-`;
-
 const Graphql = ({}: Props) => {
+  const [currPage, setCurrPage] = useState(1);
+
   const variables = {
-    search: 'One Piece',
-    page: 1,
-    perPage: 20,
+    // search: 'toaru kagaku no railgun',
+    // search: 'One Piece',
+    page: currPage,
+    perPage: 5,
   };
 
-  const { loading, error, data } = useQuery(GET_ANIME, { variables });
+  const { loading, error, data } = useGetTrendingAnimeQuery({ variables });
 
   if (loading) return <div>Loading...</div>;
-  if (error) {
-    console.log('Error: ', error);
-    return <div>Something bad happened. Refresh the page to try again.</div>;
-  }
-  const media = data.Page.media;
+  if (error) return <div>Something bad happened. Refresh the page to try again.</div>;
 
-  console.log('data: ', media);
+  const allMedia = data?.Page?.media;
+  const pageInfo = data?.Page?.pageInfo;
+
+  // console.log('pageInfo: ', pageInfo);
+  // console.log('media: ', allMedia);
 
   return (
     <div>
-      {media.map((row: any) => (
+      <button onClick={() => setCurrPage(currPage + 1)}>add</button>
+      {allMedia?.map((row: any) => (
         <div key={row.id}>{row.title.romaji}</div>
       ))}
     </div>
   );
-
-  // const url = 'https://graphql.anilist.co',
-  //   options = {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       Accept: 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //       query: query,
-  //       variables: variables,
-  //     }),
-  //   };
-
-  // useEffect(() => {
-  //   fetch(url, options).then(handleResponse).then(handleData).catch(handleError);
-  // }, [variables, query]);
-
-  // return <div>halo</div>;
 };
 
 export default Graphql;
